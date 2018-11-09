1da953eba4360bab9d1201082ab290f53bc65bc4
Siyam Al Galib <galib.abdullah04@gmail.com>
2018-11-09 16:45:08 +0600

    All notes added of 09.11

---
+++ class 09.11/input_function.py
@@ -0,0 +1,27 @@
+print('Dipto vai, apnar First name likhen:\n')
+first_name = input()
+
+print('Dipto vai, apnar Last name likhen:\n')
+last_name = input()
+
+print('Dipto vai apnar name hosse:\n')
+print(first_name, last_name)
+
+#################################################################
+
+first_name = input('Dipto vai, apnar First name likhen:\n')
+
+last_name = input('Dipto vai, apnar Last name likhen:\n')
+
+#################################################################
+print('Arithmetic between two numbers:\n')
+
+input1 = int(input('Enter a number:\n'))
+input2 = int(input('Enter another number:\n'))
+
+addition = input1 + input2
+
+print('Addition: ', addition)
+
+
+
