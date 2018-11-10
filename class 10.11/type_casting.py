15d62d2bd72556ddc3b9caf540642c434dc1a8b2
Siyam Al Galib <galib.abdullah04@gmail.com>
2018-11-10 16:26:59 +0600

    Al notes added of 10.11

---
+++ class 10.11/type_casting.py
@@ -0,0 +1,56 @@
+input_1 = float(input('Enter a value:\n'))
+input_2 = float(input('Enter another value:\n'))
+
+# result = int(input_1 * input_2)
+
+print(int(input_1 * input_2))
+# print('Multiplication is:', result)
+
+############################################################
+bool(1)
+bool(0)
+bool('')
+bool('Dipto')
+bool(0.000)
+bool(0.001)
+
+#############################################################
+print('Enter a number:\n')
+numbers_1 = int(input())
+
+print('Enter another number:\n')
+numbers_2 = int(input())
+
+result = numbers_1 + numbers_2
+
+print('\nresult:', str(result))
+print(type(str(result)))
+
+print('\nresult:', result)
+print(type(result))
+
+#################### Type Casting ################################
+""" Boolean """
+# bool()
+
+""" String """
+# str()
+
+""" Arithmetic """
+# int()
+# float()
+
+""" Data Structure """
+# list()
+# tuple()
+# dict()
+# set()
+
+""" Type """
+# type()
+
+####################################################
+
+
+
+
