10aff031babf1b81429623a0cff46c1c59480a40
siyam04 <galib.abdullah04@gmail.com>
2018-11-30 16:34:59 +0600

    added

---
+++ class 30.11/string basic.py
@@ -0,0 +1,43 @@
+string1 = 'Dipto is a Pythonista'
+string2 = 'Dipto'
+
+"""Finding Length"""
+print(string1.__len__())
+print(len(string2))
+
+"""Forward Traversing"""
+print(string2[0])
+print(string2[1])
+print(string2[2])
+print(string2[3])
+print(string2[4])
+print()
+"""Reverse Traversing"""
+print(string2[-5])
+# home work
+
+"""Direct Reversing"""
+print(string2[::-1])
+
+"""String Concatenate"""
+one = 'Bangla'
+two = 'desh'
+
+pan = 'Bangladesh '
+new = 'B ang lad esh '
+
+print(pan.replace('Bangladesh', 'NewYork'))
+
+print(pan)
+
+print(new.strip('-'))
+
+print(one + two)
+
+print('Bangla' + 'desh ' + 'beautiful')
+
+print(pan.upper())
+print(pan.lower())
+
+
+
