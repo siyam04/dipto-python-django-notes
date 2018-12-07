2c74129fb03d427c1668dca602fb49e3247a9769
siyam04 <galib.abdullah04@gmail.com>
2018-12-07 18:51:47 +0600

    Class notes added

---
+++ class 07.12/string all.py
@@ -0,0 +1,67 @@
+n = 42.5678
+print('%.2f' %n)
+
+a = 'Dhaka'
+b = 'Jessore'
+j = '+'.join((a, b))
+print(j)
+
+a = 'dhaka'
+print(a.capitalize())
+
+
+a = 'dhaka'
+print(a.lower())
+
+a = 'DHAKA'
+print(a.swapcase())
+
+a = 'bangladesh'
+print(a.capitalize())
+
+a = 'bangladesh'
+print(a.count("a"))
+
+sen = "Dipto wants to travel saint-martin"
+print(sen.count('t', 12))
+print(sen.__len__())
+print(sen[12])
+print(sen[33])
+print(sen.count('t', 12 , 33))
+
+print(sen.find("t"))
+print(sen.index("t"))
+
+print(sen.find("t"))
+
+print(sen.replace("Dipto", "shanta"))
+print(sen.strip('D'))
+processed = sen.split(' ')
+print(processed)
+print(processed[0])
+print(sen[0])
+
+# number = '100'
+print(number.rjust(10))
+print(number.ljust(10))
+
+print('100'.rjust(10))
+
+print('100'.center(10))
+
+print('12'.zfill(6))
+print('-12'.zfill(6))
+
+print('{} vs {}'.format('Bangladesh', 'W. Indies'))
+
+essay = 'Siyam is a Pythonista and he wants to make Diptp also like him.'
+
+
+
+
+
+
+
+
+
+
