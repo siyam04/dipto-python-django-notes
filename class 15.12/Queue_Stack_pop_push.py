55b1c8dc74385bfa42bb176f7fb6bdd762b5acc2
siyam04 <galib.abdullah04@gmail.com>
2018-12-15 17:51:50 +0600

    class 15.12 added

---
+++ class 15.12/Queue_Stack_pop_push.py
@@ -0,0 +1,26 @@
+# li = [1, 10, 7, 8, 3, 2, 9, 5, 6, 4]
+# li.sort()
+# print(li)
+# li.pop(1) # here, 1 is index number
+# print(li)
+
+# new_list = [0] * 10
+# print(new_list)
+#
+# new_list = [8, 7] * 10
+# print(new_list)
+
+# li = [1, 10, 7, 8, 3, 2, 9, 5, 6, 4]
+# li[0] = 100
+# print(li)
+
+# STACK
+stack = []
+stack.append(3)
+stack.append(5)
+stack.append(7)
+print(stack)
+stack.pop()
+print(stack)
+
+# Queue HW
---
+++ class 15.12/lists_part-3.py
@@ -0,0 +1,15 @@
+# extend()
+# li6 = [1, 2, 3, 4, 5]
+# li6.extend(['Dipto', 6, 7, 8, 9, 10])
+# print(len(li6))
+# print(li6)
+
+# range()
+# for i in range(1, 11):
+#     print(i, '\n')
+
+# count()
+# li2 = [1, 2, 3, 4, 5, 3, 3]
+# print(li2.count(3))
+
+
