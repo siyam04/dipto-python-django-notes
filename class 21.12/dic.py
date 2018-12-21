86a306c5e23fd1144c0fe2721e8c80fd63f8a5ea
siyam04 <galib.abdullah04@gmail.com>
2018-12-21 20:09:30 +0600

    class 21.12 added

---
+++ class 21.12/dic.py
@@ -0,0 +1,18 @@
+# dic = {'key1': '777777777777', 'ke': '9999999999999999999999999999999999999999.9'}
+# print(dic, '\n', type(dic))
+# print(dic["key1"])
+# print(dic["ke"])
+
+# d = {['key']: 10, ['key']: 20}
+# print(['key'])
+
+# dic = {'Dhaka': 10, 'Bogra': 20}
+# print(dic)
+# dic['Dhaka'] = 100
+# print(dic)
+
+
+
+
+
+
---
+++ class 21.12/set.py
@@ -0,0 +1,19 @@
+# S = set()
+# print(type(S))
+# S = set(1, 2, 3)
+
+# T = (1, 2, 3)
+# S = set(T)
+# print(S)
+# print(type(S))
+
+# li = [1, 2, 3]
+# s = set(li)
+# print(s, '\n', type(s))
+
+# S = {1, 2, 4}
+# print(type(S))
+# t = tuple(S)
+# print(t, '\n', type(t))
+
+
