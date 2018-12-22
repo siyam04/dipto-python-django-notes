64587a9cf1ecd3077fdd5e9e801ce3c2f0d0b7ff
siyam04 <galib.abdullah04@gmail.com>
2018-12-22 15:28:42 +0600

    class 22.12 added

---
+++ class 22.12/condition practice.py
@@ -0,0 +1,33 @@
+# age = int(input('Enter an Age: \n'))
+#
+# if age < 25 :
+#     print(age, '\t', 'Dipto')
+# else:
+#     print(age, '\t', 'Siyam')
+
+# a = int(input("number 1:"))
+# b = int(input("number 2:"))
+# c = int(input("number 3:"))
+#
+# if a > b :
+#     if a >c :
+#         print(a)
+# elif b > c:
+#     print(b)
+# else:
+#     print(c)
+
+# char = input('Enter a Single Character: \n')
+#
+# if char >= 'a' and char <= 'z':
+#     print('Lower Case')
+# elif char >= 'A' and char <= 'Z':
+#     print('Upper Case')
+# else:
+#     print('Nothing')
+
+# HomeWork: Leap Year Using Nested Condition
+# HomeWork: break, continue
+
+
+
