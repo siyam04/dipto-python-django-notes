f34bf6b67c527c9a1af0ef11190a5a0c04dcc93a
siyam04 <galib.abdullah04@gmail.com>
2019-01-17 14:05:02 +0600

    All Notes Added

---
+++ class 17.01.19/Exception Handling.py
@@ -0,0 +1,67 @@
+### basic
+## 1
+try:
+  print(x)
+except:
+  print("An exception occurred!")
+
+## 2
+try:
+  print(x)
+except NameError:
+  print("Variable x is not defined")
+except:
+  print("Something else went wrong")
+
+## 3
+try:
+    1 / 0
+except:
+    print("You cannot divide by zero!")
+
+## 4
+try:
+    1 / 0
+except ZeroDivisionError:
+    print("You cannot divide by zero!")
+
+
+### Try, Except, Else
+try:
+  print("Hello")
+except:
+  print("Something went wrong")
+else:
+  print("Nothing went wrong")
+
+
+### Try, Except, Finally
+## 1
+try:
+  print(x)
+except:
+  print("Something went wrong")
+finally:
+  print("The 'try except' is finished")
+
+## 2
+try:
+  f = open("test.txt")
+  # f = open("test.txt", 'w')
+  f.write("Demo Text")
+except:
+  print("Something went wrong when writing to the file")
+finally:
+  f.close()
+
+
+### Using Dictionary
+my_dict = {"a": 1, "b": 2, "c": 3}
+try:
+    value = my_dict["d"]
+except IndexError:
+    print("This index does not exist!")
+except KeyError:
+    print("This key is not in the dictionary!")
+except:
+    print("Some other error occurred!")
