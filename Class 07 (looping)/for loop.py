bfada5955356f3e6032701e9d58723457f6e4549
siyam04 <galib.abdullah04@gmail.com>
2019-03-05 12:41:31 +0600

    All Notes Added.

--- Class 07 (looping)/for loop.py
+++ Class 07 (looping)/for loop.py
@@ -4,18 +4,14 @@ input2 = int(input('Enter another number: \n'))
 for i in range(input1, input2):
     print(i)
 
+#################################################################
+
 input3 = int(input('Enter a number: \n'))
 
 for i in range(0, input3):
     print(i)
 
-test_case = 2
-
-while test_case <= 2:
-    input4 = int(input('Enter Number: \n'))
-
-    print('test case:', input4, '\n')
-
+#################################################################
 
 L = []
 
@@ -27,19 +23,26 @@ for i in range(input1, input2):
 
 print(L)
 
+#################################################################
+
 L = [1, 2, 3, 4, 5]
 casted =(L)
 
 for i in L:
     print(i)
 
+#################################################################
+
 dic = {1: 'Siyam', 2: 'Dipto', 3: 'Barna'}
 for K, V in dic.items():
     print('{}: {}'.format(K, V))
+
+#################################################################
 
 dic = {1: 'Siyam', 2: 'Dipto', 3: 'Barna'}
 
 for Keys, Values in dic.items():
     print('{', Keys, ':', Values, '}')
 
+#################################################################
 
---
+++ Class 09 (DS, list part - 2)/list_comprehension.py
@@ -0,0 +1,38 @@
+# Syntax of List Comprehension
+# [expression for item in list]
+
+#################################################################
+
+# Iterating through a string Using for Loop
+data = []
+
+for letter in 'human':
+    data.append(letter)
+
+print(data)
+
+######################### Same code using List Comprehension ########################
+
+# Iterating through a string Using List Comprehension
+data = [ letter for letter in 'human' ]
+print( data)
+
+#################################################################
+
+# Using if with List Comprehension
+number_list = [ x for x in range(20) if x % 2 == 0]
+print(number_list)
+
+#################################################################
+
+# Nested IF with List Comprehension
+num_list = [y for y in range(100) if y % 2 == 0 if y % 5 == 0]
+print(num_list)
+
+#################################################################
+
+# if...else With List Comprehension
+obj = ["Even" if i % 2 == 0 else "Odd" for i in range(10)]
+print(obj)
+
+#################################################################
