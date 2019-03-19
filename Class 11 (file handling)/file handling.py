a4cc1a9a0d76a8be0ababab79d1a6d76cf334ba4
siyam04 <galib.abdullah04@gmail.com>
2019-03-19 12:20:01 +0600

    Content Updated

--- Class 11 (file handling)/file handling.py
+++ Class 11 (file handling)/file handling.py
@@ -1,48 +1,60 @@
 ## File Read
-my_file = open('test.txt', 'r')
-my_file_2 = open('C:\\Users\\SSD-Siyam\\Desktop\\test.txt', 'r') # This line is for Path
-content = my_file.read()
-print(content)
-my_file.close()
-
-
-## File Write
-my_file = open('test_write.txt', 'w')
+# my_file = open('test.txt', 'r')
 # my_file_2 = open('C:\\Users\\SSD-Siyam\\Desktop\\test.txt', 'r') # This line is for Path
-content = my_file.write('Dipto is very Lazy!')
-print(content)
-my_file.close()
+random = open('C:\\Users\\SSD-Siyam\\Desktop\\random.txt', 'r')
+new = random.read()
+print(new)
+random.close()
+# content = my_file.read()
+# print(content)
+# my_file.close()
 
+#################################################################
 
-## File Append
-my_file = open('test_write.txt', 'a')
-content = my_file.write('\nDipto has a Girlfriend!')
-print(content)
-my_file.close()
+# File Write
+# my_file = open('test_write.txt', 'w')
+# # my_file_2 = open('C:\\Users\\SSD-Siyam\\Desktop\\test.txt', 'r') # This line is for Path
+# content = my_file.write('I love Python!')
+# print(content)
+# my_file.close()
 
+#################################################################
 
-## File Operations
-my_file = open('test.txt', 'r')
+# File Append
+# my_file = open('test_write.txt', 'a')
+# content = my_file.write('\nI also like Django!')
+# print(content)
+# my_file.close()
+
+#################################################################
+
+# File Operations
+# my_file = open('test.txt', 'r')
 # 1
-content = my_file.read(5)
-print(content)
+# content = my_file.read(5)
+# print(content)
+
 # 2
-content = my_file.read()
-print(content)
+# content = my_file.read()
+# print(content)
+
 # Pointer's position finding
-position = my_file.tell()
-print(position)
+# position = my_file.tell()
+# print(position)
+
 # Pointer starts from beginning
-my_file.seek(0, 0)
+# my_file.seek(0, 0)
+
 # Again total content
-content = my_file.read()
-print(content)
+# content = my_file.read()
+# print(content)
+
 # File closing
-my_file.close()
+# my_file.close()
 
 
-## if we use 'with', then no need to closing File
-with open('test.txt', 'r') as my_file:
-    content = my_file.read()
-    print(content)
+# if we use 'with', then no need to closing File
+# with open('test.txt', 'r') as my_file:
+#     content = my_file.read()
+#     print(content)
 
--- Class 12 (exception handling)/Exception Handling.py
+++ Class 12 (exception handling)/Exception Handling.py
@@ -1,70 +1,108 @@
 ### basic
 ## 1
-x = 1
-try:
-  print(x)
-except:
-  print("An exception occurred!")
+# x = 1
+#
+# try:
+#   print(x)
+#
+# except:
+#   print("An exception occurred!")
+
+#################################################################
 
 ## 2
-try:
-  print(x)
-except NameError:
-  print("Variable x is not defined")
-except:
-  print("Something else went wrong")
+
+# x = 2
+#
+# try:
+#   print(x)
+#
+# except NameError:
+#   print("Variable x is not defined")
+#
+# except:
+#   print("Something else went wrong")
+
+#################################################################
 
 ## 3
-try:
-    1 / 0
-except:
-    print("You cannot divide by zero!")
 
-## 4
-try:
-    1 / 0
-except ZeroDivisionError:
-    print("You cannot divide by zero!")
+# try:
+#     1 / 0
+#
+# except:
+#     print("You cannot divide by zero!")
 
+#################################################################
+
+# 4
+# try:
+#     1 / 0
+#
+# except ZeroDivisionError:
+#     print("You cannot divide by zero!")
+
+#################################################################
 
 ### Try, Except, Else
-try:
-  print("Hello")
-except:
-  print("Something went wrong")
-else:
-  print("Nothing went wrong")
 
+# try:
+#   print("Hello")
+#
+# except:
+#   print("Something went wrong")
+#
+# else:
+#   print("Nothing went wrong")
+
+#################################################################
 
 ### Try, Except, Finally
 ## 1
-x = 1
-try:
-  print(x)
-except:
-  print("Something went wrong")
-finally:
-  print("The 'try except' is finished")
+
+# x = 1
+#
+# try:
+#   print(x)
+#
+# except:
+#   print("Something went wrong")
+#
+# finally:
+#   print("The 'try except' is finished")
+
+#################################################################
 
 ## 2
-try:
-  # f = open("test.txt")
-  f = open("test.txt", 'w')
-  f.write("Demo Text")
-  # f.read()
-except:
-  print("Something went wrong when writing to the file")
-finally:
-  f.close()
 
+# try:
+#   # f = open("test.txt")
+#   f = open("test.txt", 'w')
+#   f.write("This is a sentence.")
+#   # f.read()
+#
+# except:
+#   print("Something went wrong when writing to the file")
+#
+# finally:
+#   f.close()
+
+#################################################################
 
 ### Using Dictionary
+
 my_dict = {"a": 1, "b": 2, "c": 3}
+
 try:
     value = my_dict["d"]
+    print(value)
+
 except IndexError:
     print("This index does not exist!")
+
 except KeyError:
     print("This key is not in the dictionary!")
+
 except:
     print("Some other error occurred!")
+
